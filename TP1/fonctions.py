8dd5e0f646cc79a0b40ed155f67ba98120a89568
thaust-5451678 <theomardon6@gmail.com>
2026-09-08 16:53:25 +0200

    Exception puissance

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,7 @@
 def puissance(a,b):
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Nombres entiers seulement")
-	return a**b
+	try:
+		return a**b
+	except ZeroDivisionError:
+		return ("0^"+str(b)+" est indéfini.")
--- TP1/test.py
+++ TP1/test.py
@@ -11,3 +11,5 @@ def test_2():
 	assert f.puissance(-1,-1) == -1
 	assert f.puissance(-1,-2) == 1
 	assert f.puissance(-2,-1) == -0.5
+	assert f.puissance(0,3) == 0
+	assert f.puissance(0,-5) == "0^-5 est indéfini."
