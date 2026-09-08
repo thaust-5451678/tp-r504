d06649f65acf6fee377c077f924cf7449a6ec83a
thaust-5451678 <theomardon6@gmail.com>
2026-09-08 17:28:50 +0200

    Puissances boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,7 +1,15 @@
 def puissance(a,b):
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Nombres entiers seulement")
-	try:
-		return a**b
-	except ZeroDivisionError:
+
+	if a == 0 and b < 0:
 		return ("0^"+str(b)+" est indéfini.")
+
+	res = a
+
+	for i in range(1,abs(b)):
+		res *= a
+
+	if b < 0:
+		return 1/res
+	return res
--- TP1/test.py
+++ TP1/test.py
@@ -11,5 +11,13 @@ def test_2():
 	assert f.puissance(-1,-1) == -1
 	assert f.puissance(-1,-2) == 1
 	assert f.puissance(-2,-1) == -0.5
+
+def test_3():
 	assert f.puissance(0,3) == 0
 	assert f.puissance(0,-5) == "0^-5 est indéfini."
+
+def test_4():
+	with pytest.raises(TypeError):
+		f.puissance(1.1,1)
+		f.puissance(1,1.1)
+		f.puissance(2.2,2.2)
